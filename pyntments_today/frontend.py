fb3d55095cfb668ded23049f0946af3c555915e2
PythBuster <pythbuster@gmail.com>
2024-09-24 03:07:48 +0200

    feat: add black and isort + format code

--- pyntments_today/frontend.py
+++ pyntments_today/frontend.py
@@ -3,17 +3,16 @@ import time
 
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
-
-from nicegui import ui, events
+from nicegui import events, ui
 
 from pyntments_today.constants import WORKING_DIR
+
 
 def save_appointments(appointments: list[dict]):
     print("save...")
     appointments_json_file_path = WORKING_DIR / "appointments.json"
-    appointments_json_file_path.write_text(
-        json.dumps(appointments, indent=4)
-    )
+    appointments_json_file_path.write_text(json.dumps(appointments, indent=4))
+
 
 def load_appointments() -> list[dict]:
     print("load...")
@@ -23,15 +22,22 @@ def load_appointments() -> list[dict]:
 
 def init(fastapi_app: FastAPI) -> None:
     columns = [
-        {'name': 'time', 'label': 'TIME', 'field': 'time', 'align': 'left'},
-        {'name': 'appointment', 'label': 'APPOINTMENT', 'field': 'appointment', 'align': 'left'},
+        {"name": "time", "label": "TIME", "field": "time", "align": "left"},
+        {
+            "name": "appointment",
+            "label": "APPOINTMENT",
+            "field": "appointment",
+            "align": "left",
+        },
     ]
     rows = load_appointments()
 
-    @ui.page('/appointments')
+    @ui.page("/appointments")
     async def show_appointments():
-        table = ui.table(columns=columns, rows=rows, row_key='time').classes("w-full")
-        table.add_slot('body', r'''
+        table = ui.table(columns=columns, rows=rows, row_key="time").classes("w-full")
+        table.add_slot(
+            "body",
+            r"""
                  <q-tr :props="props">
                      <q-td key="time" :props="props" class="font-bold">
                          {{ props.row.time }}
@@ -41,43 +47,47 @@ def init(fastapi_app: FastAPI) -> None:
                          <span style="white-space: pre;">{{ props.row.appointment }}</span>
                      </q-td>
                  </q-tr>
-             ''')
+             """,
+        )
 
-
-    @ui.page('/appointments/edit')
+    @ui.page("/appointments/edit")
     async def edit_appointments():
         def add_row() -> None:
-            new_id = max((dx['id'] for dx in rows), default=-1) + 1
-            rows.append({'id': new_id, 'time': '... - ...', 'appointment': '...'})
-            ui.notify(f'Added new row with ID {new_id}')
+            new_id = max((dx["id"] for dx in rows), default=-1) + 1
+            rows.append({"id": new_id, "time": "... - ...", "appointment": "..."})
+            ui.notify(f"Added new row with ID {new_id}")
             table.update()
             save_appointments(rows)
 
-
         def rename(e: events.GenericEventArguments) -> None:
             for row in rows:
-                if row['id'] == e.args['id']:
+                if row["id"] == e.args["id"]:
                     row.update(e.args)
-            ui.notify(f'Updated rows to: {table.rows}')
+            ui.notify(f"Updated rows to: {table.rows}")
             table.update()
             save_appointments(rows)
 
         def delete(e: events.GenericEventArguments) -> None:
-            rows[:] = [row for row in rows if row['id'] != e.args['id']]
+            rows[:] = [row for row in rows if row["id"] != e.args["id"]]
             ui.notify(f'Deleted row with ID {e.args["id"]}')
             table.update()
             save_appointments(rows)
 
-        table = ui.table(columns=columns, rows=rows, row_key='time').classes("w-full")
-        table.add_slot('header', r'''
+        table = ui.table(columns=columns, rows=rows, row_key="time").classes("w-full")
+        table.add_slot(
+            "header",
+            r"""
             <q-tr :props="props">
                 <q-th/>
                 <q-th v-for="col in props.cols" :key="col.name" :props="props">
                     {{ col.label }}
                 </q-th>
             </q-tr>
-        ''')
-        table.add_slot('body', r'''
+        """,
+        )
+        table.add_slot(
+            "body",
+            r"""
             <q-tr :props="props">
                 <q-td>
                     <q-btn size="sm" color="warning" round dense icon="delete"
@@ -101,15 +111,17 @@ def init(fastapi_app: FastAPI) -> None:
                     </q-popup-edit>
                 </q-td>
             </q-tr>
-        ''')
-        with table.add_slot('bottom-row'):
-            with table.cell().props('colspan=3'):
-                ui.button('Add row', icon='add', color='accent', on_click=add_row).classes('w-full')
-        table.on('rename', rename)
-        table.on('delete', delete)
-
+        """,
+        )
+        with table.add_slot("bottom-row"):
+            with table.cell().props("colspan=3"):
+                ui.button(
+                    "Add row", icon="add", color="accent", on_click=add_row
+                ).classes("w-full")
+        table.on("rename", rename)
+        table.on("delete", delete)
 
     ui.run_with(
         fastapi_app,
-        mount_path='',  # NOTE this can be omitted if you want the paths passed to @ui.page to be at the root
+        mount_path="",  # NOTE this can be omitted if you want the paths passed to @ui.page to be at the root
     )
--- pyntments_today/main.py
+++ pyntments_today/main.py
@@ -6,5 +6,5 @@ app = FastAPI()
 
 frontend.init(app)
 
-if __name__ == '__main__':
-    uvicorn.run("pyntments_today.main:app", host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    uvicorn.run("pyntments_today.main:app", host="0.0.0.0", port=8000)
